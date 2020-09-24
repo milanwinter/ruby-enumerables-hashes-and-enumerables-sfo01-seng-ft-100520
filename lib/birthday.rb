3cd172119715f29e95c793761588b806e14050ff
Milan Winter <milanwinter8@gmail.com>
2020-09-24 00:01:48 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,5 +5,7 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+   birthday_kids.each do |kid, age|
+     puts "Happy Birthday #{kid}! You are now #{age} years old!"
+   end
 end
